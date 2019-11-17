2da7640cba967422726b8140de5fd3ae7f1428fe
Dick James <rnulanz@gmail.com>
2019-11-17 11:21:21 -0600

    having trouble making articles display

--- controllers/controller.js
+++ controllers/controller.js
@@ -43,13 +43,12 @@ module.exports = function (app) {
             result.artSin = artSin;
 
             // Using our Article model, create a new entry
-            Article.create(result, function (err, doc) {
+            Article.create(result, function (error, data) {
               // Log any errors
                 if (err) {
-                console.log(err// Or log the doc
-                );
+                console.log(error);
                 } else {
-                console.log(doc);
+                console.log(data);
                 }
             });
             }
@@ -63,17 +62,34 @@ module.exports = function (app) {
   app.get("/articles", function (req, res) {
     // Grab every doc in the Articles array
     Article
-      .find({}, function (error, doc) {
+      .find({}, function (error, data) {
         // Log any errors
         if (error) {
-          console.log(error// Or send the doc to the browser as a json object
-          );
+          console.log(error);
         } else {
-          res.render("index", {result: doc});
+          res.render("index", {result: data});
         }
         //Will sort the articles by most recent (-1 = descending order)
       })
       .sort({'_id': -1});
   });
 
+    // Grab an article by it's ObjectId
+    app.get("/articles/:id", function (req, res) {
+      // Using the id passed in the id parameter, prepare a query that finds the
+      // matching one in our db...
+      Article.findOne({"_id": req.params.id})
+      // ..and populate all of the comments associated with it
+        .populate("comment")
+      // now, execute our query
+        .exec(function (error, data) {
+          // Log any errors
+          if (error) {
+            console.log(error)
+          } else {
+            res.render("comments", {result: data});
+            // res.json (doc);
+          }
+        });
+    });
 }
